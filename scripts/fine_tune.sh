67b16ed400844147166d97a50ac4f8bcb4f00db3
armaneshaghi <arman.eshaghi@gmail.com>
2023-04-05 14:34:05 +0100

    updating scripts

--- scripts/fine_tune.sh
+++ scripts/fine_tune.sh
@@ -4,5 +4,6 @@ docker run --gpus all --ipc=host --ulimit memlock=-1 \
 --shm-size="10gb" --ulimit stack=67108864 -v ${working_dir}:/mnt \
 -v /mounts/auto/arman7:/mounts/auto/arman7:shared  \
 --network=host --rm -it -e NCCL_DEBUG=INFO \
-armaneshaghi/mind-glide:latest \
-/mounts/auto/arman7/workflows/mindGlide/scripts/fine_tune_from_container.sh
+--entrypoint /mounts/auto/arman7/workflows/mindGlide/scripts/fine_tune_from_container.sh \
+armaneshaghi/mind-glide:latest 
+
