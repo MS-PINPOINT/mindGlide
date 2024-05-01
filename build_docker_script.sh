f0e3331139deb066e4135d4b314a6b367cb77fb8
phili <e1242252@student.tuwien.ac.at>
2024-05-01 12:07:34 +0100

    Debugging action

--- build_docker_script.sh
+++ build_docker_script.sh
@@ -1,1 +1,1 @@
-docker build  --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg UNAME="user" -t mspinpoint/mind-glide:aug2024 .
+docker build  --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg UNAME="user" -t mspinpoint/mindglide:may2024 .
