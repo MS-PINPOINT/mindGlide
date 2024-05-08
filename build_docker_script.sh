2d7c31d4d83edcd3665db3fb33151941ad805a1a
phili <e1242252@student.tuwien.ac.at>
2024-05-08 11:47:04 +0100

    Added git lfs to actions

--- build_docker_script.sh
+++ build_docker_script.sh
@@ -1,1 +1,1 @@
-docker build  --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg UNAME="user" -t mspinpoint/mindglide:may2024 .
+docker build  --build-arg USER_ID=$(id -u) --build-arg GROUP_ID=$(id -g) --build-arg UNAME="user" -t mspinpoint/mindglide:test2024 .
