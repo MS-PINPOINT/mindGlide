f8b5baf219c1152c990e1d71acb218c7b8d5906e
phili <e1242252@student.tuwien.ac.at>
2024-06-11 09:29:26 +0100

    edited Dockerfile and requirements

--- Dockerfile
+++ Dockerfile
@@ -5,23 +5,42 @@ ENV DEBIAN_FRONTEND=noninteractive
 ARG USER_ID
 ARG GROUP_ID
 ARG UNAME
-RUN groupadd -g $GROUP_ID -o $UNAME
-RUN useradd -m -u $USER_ID -g $GROUP_ID -o -s /bin/bash $UNAME
-RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC
-RUN apt-get update --allow-unauthenticated
-RUN apt-get install -y git
+
+# Create user and group with specified IDs
+RUN groupadd -g $GROUP_ID -o $UNAME && \
+    useradd -m -u $USER_ID -g $GROUP_ID -o -s /bin/bash $UNAME
+
+# Install dependencies
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys A4B469963BF863CC && \
+    apt-get update --allow-unauthenticated && \
+    apt-get install -y git
+
+# Set up the virtual environment
 WORKDIR /opt
-RUN mkdir /opt/virtualenv
-RUN python3 -m venv /opt/virtualenv
+RUN mkdir /opt/virtualenv && \
+    python3 -m venv /opt/virtualenv
 COPY requirements.txt /opt/virtualenv
-RUN /opt/virtualenv/bin/pip install --upgrade pip
-RUN /opt/virtualenv/bin/pip install -r /opt/virtualenv/requirements.txt
+RUN /opt/virtualenv/bin/pip install --upgrade pip && \
+    /opt/virtualenv/bin/pip install -r /opt/virtualenv/requirements.txt
+
+# Clone the MONAI tutorials repository
 RUN git clone https://github.com/Project-MONAI/tutorials.git /opt/monai-tutorials
-WORKDIR /opt/monai-tutorials 
+WORKDIR /opt/monai-tutorials
 RUN git checkout c501cbef2c291b4920b9a8ad3e4a67f334f79f30
+
+# Copy configuration file
 COPY mindGlide/config/task_params.py /opt/monai-tutorials/modules/dynunet_pipeline/
-WORKDIR /mnt
+
+# Copy entrypoint script and make it executable
+COPY scripts/entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
+
+# Switch to non-root user
 USER $UNAME
-COPY scripts/entrypoint.sh /entrypoint.sh 
+
+# Set working directory and copy application files
+WORKDIR /mnt
 COPY ./ /opt/mindGlide
-ENTRYPOINT [ "/entrypoint.sh" ]
+
+# Set entrypoint
+ENTRYPOINT ["/entrypoint.sh"]
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,34 +1,28 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Activate the virtual environment
+source /opt/virtualenv/bin/activate
+
+# Check if scan path is provided
 scan_path="$1"
 if [ -z "$scan_path" ]; then
     echo "Please provide a scan path, cannot find it or is empty."
     echo "Usage: infer_ensemble.sh <scan_path>"
     exit 1
 fi
-model_path="/opt/mindGlide/models/_20240404_conjurer_trained_dice_7733.pt"
-#model_path1="/opt/mindGlide/models/model_0_net_key_metric=0.7627.pt"
-#model_path2="/opt/mindGlide/models/model_2_net_key_metric=0.7541.pt"
-#model_path3="/opt/mindGlide/models/model_2_net_key_metric=0.7579.pt"
-#model_path4="/opt/mindGlide/models/model_3_net_key_metric=0.7713.pt"
-#model_path5="/opt/mindGlide/models/model_3_net_key_metric=0.7717.pt"
-#model_path6="/opt/mindGlide/models/model_4_net_key_metric=0.7645.pt"
-#model_path7="/opt/mindGlide/models/model_5_net_key_metric=0.7866.pt"
-#model_path8="/opt/mindGlide/models/model_6_net_key_metric=0.7723.pt"
-#model_path9="/opt/mindGlide/models/model_7_net_key_metric=0.7634.pt"
-#model_path10="/opt/mindGlide/models/model_8_net_key_metric=0.7489.pt"
-#model_path11="/opt/mindGlide/models/model_9_net_key_metric=0.7738.pt"
 
-export PYTHONPATH="/opt/:/opt/monai:${PYTHONPATH}"
-command="python /opt/mindGlide/mindGlide/run_inference.py  --model_file_paths  ${model_path} \
---scan_path ${scan_path}"
-#command="python /opt/mindGlide/mindGlide/run_inference.py  --model_file_paths  ${model_path1} ${model_path2} \
-#${model_path3} ${model_path4} ${model_path5} ${model_path6} ${model_path7} \
-#${model_path8} ${model_path9} ${model_path10} ${model_path11} ${model_path12} \
-#${model_path13} \
-#--scan_path ${scan_path}"
-#command="python /opt/mindGlide/mindGlide/run_inference.py  --model_file_paths  ${model_path1} \
-#${model_path2} \
-#${model_path13} \
-#--scan_path ${scan_path}"
+# Define the model path
+model_path="/opt/mindGlide/models/_20240404_conjurer_trained_dice_7733.pt"
+
+# Set the PYTHONPATH
+export PYTHONPATH="/opt:/opt/monai:${PYTHONPATH}"
+
+# Define the command to run
+command="python /opt/mindGlide/mindGlide/run_inference.py --model_file_paths ${model_path} --scan_path ${scan_path}"
+
+# Print and execute the command
 echo $command
-$command
+exec $command
